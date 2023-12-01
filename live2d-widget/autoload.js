4d7f313f3a8a57064dccd6a1d33417a69ef2fe49
Dragonzsl <g1335026358@gmail.com>
2023-12-01 22:16:58 +0800

    Site updated: 2023-12-01 22:16:58

---
+++ live2d-widget/autoload.js
@@ -0,0 +1,19 @@
+const live2d_path="/live2d-widget/";function loadExternalResource(o,s){return new Promise((e,i)=>{let t;if(s==="css"){t=document.createElement("link");t.rel="stylesheet";t.href=o}else if(s==="js"){t=document.createElement("script");t.src=o}if(t){t.onload=()=>e(o);t.onerror=()=>i(o);document.head.appendChild(t)}})}if(screen.width>=768){Promise.all([loadExternalResource(live2d_path+"waifu.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",apiPath:"https://api.moechun.fun/php/live2d_api/",tools:["hitokoto","asteroids","switch-model","switch-texture","photo","info","quit"]})})}console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
